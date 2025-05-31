179764ac8e95d59288bb393c2ca0fc897559300c
Proha223 <152096566+Proha223@users.noreply.github.com>
2025-05-31 22:57:51 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,14 +11,16 @@ internal class Program
         try
         {
             string token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")
-                         ?? "7280957738:AAEiepxLyZfj667V60Q9-4Rs94Vn5uW9ie8";
+               ?? throw new InvalidOperationException("TELEGRAM_BOT_TOKEN переменная окружения не задана в Railway");
+            /*string token = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")
+                         ?? "Телеграм_Токен";*/ // Для локального тестирования 
 
             mybot = new Host(token);
             mybot.Start();
             mybot.OnMessage += OnMessage;
 
-            //Console.ReadLine(); // раскоментить для локального тестирования => комментить некст строчку
-            Thread.Sleep(Timeout.Infinite);
+            Console.WriteLine("Бот запущен. Для остановки нажмите Enter...");
+            Console.ReadLine(); // Оставляем для локального тестирования
         }
         catch (Exception ex)
         {
@@ -26,6 +28,7 @@ internal class Program
         }
     }
 
+    // Ваш существующий код обработки сообщений остается без изменений
     private static async void OnMessage(ITelegramBotClient client, Update update)
     {
         if (update.Message?.Text == null || update.Message?.Chat == null) return;
